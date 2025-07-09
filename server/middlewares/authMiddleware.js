853efe27c536a44854376779d97b55dd89cfbf7f
akshaykatariia <akshaykataria055@gmail.com>
2025-07-09 19:19:09 +0530

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,18 +1,19 @@
-import { clerkClient } from "@clerk/express"
+import { clerkClient } from "@clerk/express";
 
-// Middleware ( Protect Educator Routes )
-export const protectEducator = async (req, res, next) => {
-  try {
-    const userId = req.auth.userId
-    const response = await clerkClient.users.getUser(userId)
+// middleware protect educator routes
 
-    if (response.publicMetadata.role !== 'educator') {
-      return res.json({ success: false, message: 'Unauthorized Access' })
+export const protectEducator=async(req,res,next)=>{
+    try {
+        const userId=req.auth.userId
+        const response =await clerkClient.users.getUser(userId);
+        // console.log("response is ",response);
+        if(response.publicMetadata.role !== 'educator'){
+            return res.json({success:false,message:'UnAuthorized Access'})
+        }
+
+        next()
+
+    } catch (error) {
+        res.json({success:false,message:error.mmessage})
     }
-
-    next()
-
-  } catch (error) {
-    res.json({ success: false, message: error.message })
-  }
 }
