ba7df95451fe81de4b1191032c5f5186346807f5
akshaykatariia <akshaykataria055@gmail.com>
2025-07-09 19:17:14 +0530

    Update courseController.js

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -1,73 +1,36 @@
-import express from "express";
 import Course from "../models/Course.js";
-import User from "../models/User.js";
 
+// Get all Courses
 
-//get all courses
-export const getAllCourses = async (req, res) => {
-  try {
-    const courses = await Course.find({ isPublished: true })
-      .select(["-courseContent", "-enrolledStudents"])
-      .populate({ path: "educator" });
+export const getAllCourses = async(req,res)=>{
+    try {
+        const courses = await Course.find({isPublished:true}).select(['-courseContent','-enrolledStudents']).populate({path:'educator'})
 
-    res.json({ success: true, courses });
-  } catch (error) {
-    res.json({ success: false, message: error.message });
-  }
-};
-
-//get course by id
-export const getCourseId = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const courseData = await Course.findById(id).populate({ path: "educator" });
-
-    //remove lectureurl if preview free is false
-    courseData.courseContent.forEach((chapter) => {
-      chapter.chapterContent.forEach((lecture) => {
-        if (!lecture.isPreviewFree) {
-          lecture.lectureUrl = "";
-        }
-      });
-    });
-
-    res.json({ success: true, courseData });
-  } catch (error) {
-    res.json({ success: false, message: error.message });
-  }
-};
-
-const router = express.Router();
-
-
-export const purchaseCourse = async (req, res) => {
-
-  try {
-    const { courseId } = req.body;
-    const { origin } = req.headers;
-    const userId = req.auth.userId;
-    const userData = await User.findById(userId);
-    const courseData = await Course.findById(courseId);
-
-    if (!userData || !courseData) {
-      return res.json({ success: false, message: "Data Not Found" });
+        res.json({success:true,courses});
+    } catch (error) {
+        res.json({success:true,messag:error.message});
     }
+}
 
-    const purchaseData = {
-        courseId : courseData._id,
-      userId,
-      amount:(courseData.coursePrice - courseData.discount *  courseData.coursePrice / 100).toFixed(2)
-      
+// Get course by id 
+
+export const getCourseId=async (req,res)=>{
+    const {id} = req.params
+    try {
+        const courseData= await Course.findById(id).populate({path:'educator'})
+
+        //Remove lecture url if isPreview is false
+        courseData.courseContent.forEach(chapter=>{
+            chapter.chapterContent.forEach(lecture=>{
+                if(!lecture.isPreviewFree){
+                    lecture.lectureUrl="";
+                }
+            }) 
+        })
+        
+        res.json({success:true,courseData})
+    } catch (error) {
+        res.json({success:false,message:error.message});
     }
-
-const newPurchase = await Purchase.create(purchaseData)
-
-// Stripe Gateway Initialize
-const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
-
-
-  } catch (error) {}
-};
-
+}
 
