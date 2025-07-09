73e81bcb790a0d097a9df9ac73f5db97c4221150
akshaykatariia <akshaykataria055@gmail.com>
2025-07-09 19:21:31 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,16 +1,15 @@
-import express from "express";
-import {
-  getUserData,
-  userEnrolledCourses,
-} from "../controllers/userController.js";
-import { purchaseCourse } from "../controllers/courseController.js";
-import { requireAuth } from "@clerk/express";
-
+import express from 'express'
+import { addUserRatings, getUserCourseProgress, getUserData, purchaseCourse, UpdateuserCourseProgress, userEnrolledCourses } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
-userRouter.get("/data", getUserData);
-userRouter.get("/enrolled-courses", userEnrolledCourses);
-userRouter.post("/purchase", requireAuth(), purchaseCourse);
+userRouter.get('/data',getUserData)
+userRouter.get('/enrolled-courses',userEnrolledCourses)
+userRouter.post('/purchase',purchaseCourse);
+
+userRouter.post('/update-course-progress',UpdateuserCourseProgress);
+userRouter.post('/get-course-progress',getUserCourseProgress);
+userRouter.post('/add-rating',addUserRatings);
+
 
 export default userRouter;
