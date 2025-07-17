4acf3b8a230e48da0581f04974e7401cfb478178
akshaykatariia <akshaykataria055@gmail.com>
2025-07-17 19:55:29 +0530

    Update courseRoute.js

--- server/routes/courseRoute.js
+++ server/routes/courseRoute.js
@@ -1,10 +1,9 @@
-import express from 'express';
-import { getAllCourses, getCourseId } from '../controllers/courseController.js';
+import express from 'express'
+import { getAllCourses, getCourseId } from '../controllers/courseController.js'
 
-const courseRouter = express.Router()
+const courseRouter=express.Router()
 
-courseRouter.get("/all", getAllCourses) // Jab frontend /courses/all pe request karega (GET request), toh getAllCourses method chalega.
-courseRouter.get("/:id", getCourseId)
+courseRouter.get('/all',getAllCourses)
+courseRouter.get('/:id',getCourseId)
 
 export default courseRouter;
-
