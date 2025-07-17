4b788bbd18921856ca226f66f9eedf23a7021b23
akshaykatariia <akshaykataria055@gmail.com>
2025-07-17 19:33:56 +0530

    Update Educator.jsx

--- Client/src/pages/educator/Educator.jsx
+++ Client/src/pages/educator/Educator.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from '../../components/educator/Navbar'
+import Navbar from '../../components/student/Navbar'
 import Sidebar from '../../components/educator/Sidebar'
-import Footer from '../../components/educator/Footer'
+import Footer from '../../components/student/Footer'
 
 
 const Educator = () => {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-1 p-4">
-        <Outlet /> {/* This is where nested routes like Dashboard, AddCourse will render */}
+    <div className='text-default min-h-screen bg-white'>
+      <Navbar />
+      <div className='flex'>
+        <Sidebar />
+        <div className='flex-1'>
+          {<Outlet />}
+        </div>
       </div>
+      <Footer/>
     </div>
-  );
-};
+  )
+}
 
 export default Educator
