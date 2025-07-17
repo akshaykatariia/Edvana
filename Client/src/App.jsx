fe454badab6743b64dfc5fda47cbcefbd34b400b
akshaykatariia <akshaykataria055@gmail.com>
2025-07-17 19:49:11 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,48 +1,56 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes, useMatch, Navigate } from 'react-router-dom'; 
 import Home from './pages/student/Home';
 import CoursesList from './pages/student/CoursesList';
 import CourseDetails from './pages/student/CourseDetails';
-import MyEnrollments from './pages/student/MyEnrollments';
+import MyEnrollment from './pages/student/MyEnrollments';
 import Player from './pages/student/Player';
 import Loading from './components/student/Loading';
 import Educator from './pages/educator/Educator';
 import Dashboard from './pages/educator/Dashboard';
-import AddCourses from './pages/educator/AddCourses';
-import MyCourses from './pages/educator/MyCourses';
+import Addcourse from './pages/educator/AddCourses';
+import Mycourses from './pages/educator/MyCourses';
 import StudentsEnrolled from './pages/educator/StudentsEnrolled';
+import DeleteCoursesGrid from './pages/educator/DeleteCoursesGrid';
 import Navbar from './components/student/Navbar';
-import { Navigate } from 'react-router-dom';
+import ContactUs from './pages/student/contactUs';
+import AboutUs from './pages/student/AboutUs';
+import PrivacyPolicy from './pages/student/PrivacyPolicy';
+import { ToastContainer } from 'react-toastify';
+
+import "quill/dist/quill.snow.css";
 
 
 const App = () => {
-  const location = useLocation();
-  const isEducatorRoute = location.pathname.startsWith('/educator');
-
+  const isEducatorPage=useMatch('/educator/*');
   return (
+
     <div className='text-default min-h-screen bg-white'>
-      {!isEducatorRoute && <Navbar />}
-
+      <ToastContainer/>
+      {!isEducatorPage && <Navbar/>}
       <Routes>
-        {/* Student Routes */}
-        <Route path='/' element={<Home />} />
-        <Route path='/course-list/:input' element={<CoursesList />} />
-        <Route path='/course-list' element={<CoursesList />} />
-        <Route path='/course/:id' element={<CourseDetails />} />
-        <Route path='/my-enrollments' element={<MyEnrollments />} />
-        <Route path='/player/:courseId' element={<Player />} />
-        <Route path='/loading/:path' element={<Loading />} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/contact-us' element={<ContactUs/>} />
+        <Route path='/about-us' element={<AboutUs/>} />
+        <Route path='/privacy-policy' element={<PrivacyPolicy/>} />
+        <Route path='/course-list' element={<CoursesList/>} />
+        <Route path='/course-list/:input' element={<CoursesList/>} />
+        <Route path='/course/:id' element={<CourseDetails/>} />
+        <Route path='/my-enrollment' element={<MyEnrollment/>} />
+        <Route path='/player/:courseId' element={<Player/>} />
+        <Route path='/loading/:path' element={<Loading/>} />
 
-        {/* Educator Routes */}
-        <Route path='/educator' element={<Educator />}>
-          <Route index element={<Navigate to="dashboard" replace />} />    {/*this defaults open dashboard  on clicking educator dashbaord button*/}
-          <Route path='dashboard' element={<Dashboard />} /> 
-          <Route path='add-course' element={<AddCourses />} />
-          <Route path='my-courses' element={<MyCourses />} />
-          <Route path='student-enrolled' element={<StudentsEnrolled />} />
+        <Route path='/educator' element={<Educator/>}>
+            <Route index element={<Navigate to="dashboard" replace />} />    this defaults open dashboard  on clicking educator dashbaord button
+            <Route path='dashboard' element={<Dashboard />} /> 
+            <Route path='add-course' element={<Addcourse/>}/>
+            <Route path='my-course' element={<Mycourses/>}/>
+            <Route path='delete-courses' element={<DeleteCoursesGrid/>}/>
+            <Route path='student-enrolled' element={<StudentsEnrolled/>}/>
         </Route>
       </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
