c0ada6c25ac4f079b614d453312b6bb54806442d
akshaykatariia <akshaykataria055@gmail.com>
2025-07-17 19:57:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,43 +2,40 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
-import { clerkWebhooks } from "./controllers/webhooks.js";
+import { clerkWebhooks, stripeWebhooks } from "./controllers/webhooks.js";
 import educatorRouter from "./routes/educatorRoutes.js";
 import { clerkMiddleware } from "@clerk/express";
 import connectCloudinary from "./configs/cloudinary.js";
-import path from "path";
-import { fileURLToPath } from "url";
 import courseRouter from "./routes/courseRoute.js";
 import userRouter from "./routes/userRoutes.js";
+import Stripe from "stripe";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// Initialize Stripe
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Initialize Express
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
-
-await connectDB();
-await connectCloudinary();
-
-// Middlewares
+// Middleware
 app.use(cors());
 app.use(clerkMiddleware());
 
-// Routes
-app.get("/", (req, res) => res.send("API Working"));
-app.get('/favicon.ico', (req, res) => res.status(204));
-app.post("/clerk", express.json(), clerkWebhooks);
-app.use("/api/educator", express.json(), educatorRouter); //Yeh function Clerk ke user
-//  ka role "educator" bana deta hai, taaki wo course publish kar sake.
-app.use("/api/course", express.json(), courseRouter);
-app.use("/api/user",express.json(), userRouter);
+// Connect Database & Cloudinary
+await connectDB();
+await connectCloudinary();
 
+// Routes
+app.get("/", (req, res) => res.send("API WORKING"));
+app.post("/clerk", express.json(), clerkWebhooks);
+app.use("/api/educator", express.json(), educatorRouter);
+app.use("/api/course", express.json(), courseRouter);
+app.use("/api/user", express.json(), userRouter);
+
+app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
 // Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
