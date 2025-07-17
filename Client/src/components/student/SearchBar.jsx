a133150d83b5a702513e52d42a6ab5dccad9f38e
akshaykatariia <akshaykataria055@gmail.com>
2025-07-17 19:31:09 +0530

    Update SearchBar.jsx

--- Client/src/components/student/SearchBar.jsx
+++ Client/src/components/student/SearchBar.jsx
@@ -2,41 +2,38 @@ import React, { useState } from "react";
 import { assets } from "../../assets/assets";
 import { useNavigate } from "react-router-dom";
 
-const SearchBar = ({ data }) => {
-  // idhar data prop isliye paas kiya h agar
-  // mene direct search button pe click kiya without writing anything toh ye data mein
-  //  jo hoga woh paas hoga aur ye data variable kahi aur se aa rha h
-
+const Searchbar = ({ data }) => {
   const navigate = useNavigate();
   const [input, setInput] = useState(data ? data : "");
 
-  const onSearchHandler = (e) => {
-    // e is event , form submit event
-    e.preventDefault(); // prevents full page from reloading  and makes SPA
+  const OnSearchHandler = (e) => {
+    e.preventDefault();
     navigate("/course-list/" + input);
   };
 
   return (
     <form
-      onSubmit={onSearchHandler}
-      className="max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded"
+      onSubmit={OnSearchHandler}
+      className="max-w-xl w-full md:h-14 h-12 flex item-center bg-white border border-gray-500/20 rounded"
     >
       <img
         src={assets.search_icon}
         alt="search_icon"
         className="md:w-auto w-10 px-3"
+        style={{ marginTop: "13px", height: "25px" }}
       />
       <input
-        onChange={(e) => setInput(e.target.value)}
-        value={input} // changes according to input
         type="text"
         placeholder="Search for courses"
         className="w-full h-full outline-none text-gray-500/80"
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+        value={input}
       />
-
       <button
         type="submit"
-        className="bg-blue-600 rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1"
+        className="bg-blue-600 rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1 mr-1 mt-1 md:h-[46px] h-[37px]"
       >
         Search
       </button>
@@ -44,4 +41,4 @@ const SearchBar = ({ data }) => {
   );
 };
 
-export default SearchBar;
+export default Searchbar;
