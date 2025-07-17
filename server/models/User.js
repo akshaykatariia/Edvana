a6c681102d9aae95f6a4741631a395addca70e6d
akshaykatariia <akshaykataria055@gmail.com>
2025-07-17 19:55:04 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -2,23 +2,17 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
     {
-        _id:{type: String, required:true},
-        name: {type: String, required:true},
-        email: {type: String, required:true},
-        imageUrl: {type: String, required:true},
+        _id: {type: String,required: true},
+        name: {type: String,required: true},
+        email: {type: String,required: true},
+        imageUrl: {type: String,required: true},
         enrolledCourses: [
             {
                 type: mongoose.Schema.Types.ObjectId,
                 ref: 'Course'
             }
-        ]
+        ],
+    }, {timestamps: true});
 
-    }, {timestamps: true} // Mongoose will automatically add:
-
-// createdAt – when the user was created 
-// updatedAt – when the user was last updated
-);
-
-const User = mongoose.model('User', userSchema);
-
-export default User;
+    const User = mongoose.model('User',userSchema);
+    export default User;
